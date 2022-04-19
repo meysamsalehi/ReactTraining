c023cc6f5a9017be00c3017d5ef3d57b70779d50
Meysam Salehi <m.salehi@tamitco.ir>
2022-04-19 17:54:30 +0430

    Session 6 => Create Product Component And Director for it And Render Product to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Product from "./Component/Product/Product";
+
 const App = () => {
   return (
     <div className="container" id="title">
       <h1>shopping App</h1>
-      <p>product #1 : book 1</p>
+      <Product />
+      <Product />
+      <Product />
     </div>
   );
 };
