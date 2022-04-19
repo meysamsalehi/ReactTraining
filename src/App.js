9fb3d075ac06d4d9b6042fb3dec09dcd272e133f
Meysam Salehi <m.salehi@tamitco.ir>
2022-04-19 17:45:20 +0430

    Session 5 => Component , Named & Default Export & Import

---
+++ src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="container" id="title">
+      <h1>shopping App</h1>
+      <p>product #1 : book 1</p>
+    </div>
+  );
+};
+
+export default App;
+
+export const userName = "meysam";
+export const lastName = "salehi";
