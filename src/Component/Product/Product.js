a2a8022845183b1c5e0ebe50542969f3d0ae879b
meysamsalehi33 <meysamsalehi33@gmail.com>
2022-04-20 21:52:48 +0430

    Session18 => Css module

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,7 +1,7 @@
-import "./Product.css";
+import styles from "./Product.module.css";
 const Product = (props) => {
   return (
-    <div className="product">
+    <div className={styles.product}>
       <p>product name : {props.name}</p>
       <p>product name : {props.price}</p>
       <p> discount is {props.children}</p>
