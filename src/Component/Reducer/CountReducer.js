89adc537f33a150116335a689b777f75587f1dac
Meysam Salehi <m.salehi@tamitco.ir>
2022-04-26 12:07:10 +0430

    Session 60=> trick for useReducer

--- src/Component/Reducer/CountReducer.js
+++ src/Component/Reducer/CountReducer.js
@@ -1,20 +1,12 @@
 import React, { useReducer } from "react";
 
-const initialState = {
-  firstCounter: 0,
-  secondCounter: 0,
-};
-
+const initialState = 0;
 const reducer = (state, action) => {
   switch (action.type) {
     case "add":
-      return { ...state, firstCounter: state.firstCounter + action.value };
-    case "add2":
-      return { ...state, secondCounter: state.secondCounter + action.value };
+      return state + action.value;
     case "decrement":
-      return { ...state, firstCounter: state.firstCounter - action.value };
-    case "decrement2":
-      return { ...state, secondCounter: state.secondCounter - action.value };
+      return state - action.value;
     default:
       return state;
   }
@@ -22,31 +14,36 @@ const reducer = (state, action) => {
 
 const CountReducer = () => {
   const [count, dispatch] = useReducer(reducer, initialState);
+  const [countTwo, dispatchTwo] = useReducer(reducer, initialState);
 
   return (
-    <div>
-      <h2> count first is: {count.firstCounter} </h2>
-      <h2> count second is: {count.secondCounter} </h2>
-      <button onClick={() => dispatch({ type: "add", value: 1 })}>
-        +1 One
-      </button>
-      <button onClick={() => dispatch({ type: "add", value: 5 })}>
-        +5 One
-      </button>
-      <button onClick={() => dispatch({ type: "decrement", value: 1 })}>
-        -1 One
-      </button>
-      <button onClick={() => dispatch({ type: "add2", value: 1 })}>
-        +1 Two
-      </button>
-      <button onClick={() => dispatch({ type: "add2", value: 5 })}>
-        +5 Two
-      </button>
+    <>
+      <div>
+        <h2> count first is: {count} </h2>
+        <button onClick={() => dispatch({ type: "add", value: 1 })}>
+          +1 One
+        </button>
+        <button onClick={() => dispatch({ type: "add", value: 5 })}>
+          +5 One
+        </button>
+        <button onClick={() => dispatch({ type: "decrement", value: 1 })}>
+          -1 One
+        </button>
+      </div>
 
-      <button onClick={() => dispatch({ type: "decrement2", value: 1 })}>
-        -1 Two
-      </button>
-    </div>
+      <div>
+        <h2> count first is: {countTwo} </h2>
+        <button onClick={() => dispatchTwo({ type: "add", value: 1 })}>
+          +1 One
+        </button>
+        <button onClick={() => dispatchTwo({ type: "add", value: 5 })}>
+          +5 One
+        </button>
+        <button onClick={() => dispatchTwo({ type: "decrement", value: 1 })}>
+          -1 One
+        </button>
+      </div>
+    </>
   );
 };
 
